abe14f9ee088a9baab9caeb2b9000c77864351bc
Piotr Trocki <piotr.trocki@callstack.com>
2019-12-03 14:30:44 +0100

    feat: implement ViewPager2

---
+++ android/src/main/java/com/reactnativecommunity/viewpager/ViewPagerFragment.java
@@ -0,0 +1,31 @@
+package com.reactnativecommunity.viewpager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ViewPagerFragment extends Fragment {
+
+    static String CHILD_VIEW_KEY = "CHILD_VIEW_KEY";
+
+    public static ViewPagerFragment newInstance(int id) {
+        Bundle args = new Bundle();
+        args.putInt(CHILD_VIEW_KEY, id);
+        ViewPagerFragment fragment = new ViewPagerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return ReactViewPagerManager.reactChildrenViews.get(getArguments().getInt(CHILD_VIEW_KEY));
+    }
+
+}
