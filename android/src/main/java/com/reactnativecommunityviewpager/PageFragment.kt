8beb95c1872f2006c12f0c65c8bd1ee490991ab0
Alpha <alpha0010@users.noreply.github.com>
2021-01-20 17:09:08 -0500

    fix(android): resolve memory leak

--- android/src/main/java/com/reactnativecommunityviewpager/PageFragment.kt
+++ android/src/main/java/com/reactnativecommunityviewpager/PageFragment.kt
@@ -5,42 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
-import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 
-class PageFragment : Fragment() {
-  private var mPosition: Int = -1
-  var adapter: ReactViewAdapter? = null
-
+class PageFragment(private val child: View?) : Fragment() {
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-    return FrameLayout(inflater.context)
-  }
-
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    arguments?.takeIf { it.containsKey(ARG_POSITION) }?.apply {
-      mPosition = getInt(ARG_POSITION)
+    return child?.apply {
+      layoutParams.width = MATCH_PARENT
+      layoutParams.height = MATCH_PARENT
     }
-    adapter?.registerReactPageObserver(this)
-    onReactViewUpdate()
-  }
-
-  override fun onDestroyView() {
-    (view as FrameLayout).removeAllViews()
-    adapter?.unregisterReactPageObserver(this)
-    super.onDestroyView()
-  }
-
-  fun onReactViewUpdate() {
-    val dataView = adapter?.getViewAtPosition(mPosition)
-    if (dataView != null) {
-      val rootView = view as FrameLayout
-      if (rootView.childCount == 1 && rootView.getChildAt(0) == dataView) {
-        return
-      }
-
-      (dataView.parent as FrameLayout?)?.removeAllViews()
-      rootView.removeAllViews()
-      rootView.addView(dataView, MATCH_PARENT, MATCH_PARENT)
-    }
+      ?: View(inflater.context)
   }
 }
--- android/src/main/java/com/reactnativecommunityviewpager/ReactViewAdapter.kt
+++ android/src/main/java/com/reactnativecommunityviewpager/ReactViewAdapter.kt
@@ -1,38 +1,23 @@
 package com.reactnativecommunityviewpager
 
-import android.database.Observable
-import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-
-private class AdapterObservable : Observable<PageFragment>() {
-  fun onReactViewUpdate() {
-    for (observer in mObservers) {
-      observer.onReactViewUpdate()
-    }
-  }
-}
-
-const val ARG_POSITION = "position"
+import kotlin.math.max
+import kotlin.math.min
 
 class ReactViewAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+  private var mPrevOffset = 0
+  private var mPrevReactChildrenCount = 0
   private val mReactChildrenViews = ArrayList<View>()
-  private val mObservable = AdapterObservable()
   var count = 0
   var offset = 0
 
   override fun getItemCount() = count
 
-  override fun createFragment(position: Int): Fragment {
-    val page = PageFragment()
-    page.arguments = Bundle().apply { putInt(ARG_POSITION, position) }
-    page.adapter = this
-    return page
-  }
+  override fun createFragment(position: Int) = PageFragment(getViewAtPosition(position))
 
-  fun getViewAtPosition(position: Int): View? {
+  private fun getViewAtPosition(position: Int): View? {
     return try {
       mReactChildrenViews[position - offset]
     } catch (e: IndexOutOfBoundsException) {
@@ -40,11 +25,30 @@ class ReactViewAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapte
     }
   }
 
+  fun onAfterUpdateTransaction() {
+    val changedPositions = (offset until offset + mReactChildrenViews.size).toMutableSet()
+    val bound = min(
+      count,
+      mPrevOffset + max(mPrevReactChildrenCount, mReactChildrenViews.size)
+    )
+    for (position in mPrevOffset until bound) {
+      if (changedPositions.contains(position)) {
+        changedPositions.remove(position)
+      } else {
+        changedPositions.add(position)
+      }
+    }
+
+    for (position in changedPositions) {
+      notifyItemChanged(position)
+    }
+
+    mPrevOffset = offset
+    mPrevReactChildrenCount = mReactChildrenViews.size
+  }
+
   fun addReactView(child: View, index: Int) = mReactChildrenViews.add(index, child)
   fun getReactChildAt(index: Int) = mReactChildrenViews[index]
   fun getReactChildCount() = mReactChildrenViews.size
   fun removeReactViewAt(index: Int) = mReactChildrenViews.removeAt(index)
-  fun onAfterUpdateTransaction() = mObservable.onReactViewUpdate()
-  fun registerReactPageObserver(page: PageFragment) = mObservable.registerObserver(page)
-  fun unregisterReactPageObserver(page: PageFragment) = mObservable.unregisterObserver(page)
 }
