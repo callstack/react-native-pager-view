16f77fe823bf09fb2e96a30659e6e2394c371dda
Piotr Trocki <piotr.trocki@callstack.com>
2021-04-05 13:50:27 +0200

    add typescript to detox

--- example/e2e/firstTest.e2e.ts
+++ example/e2e/firstTest.e2e.ts
@@ -1,3 +1,5 @@
+import { by, device, expect, element } from 'detox';
+
 describe('Example', () => {
   beforeAll(async () => {
     await device.launchApp();
@@ -9,10 +11,8 @@ describe('Example', () => {
 
   it('swipes to the next page', async () => {
     await element(by.text('Basic Example')).tap();
-    await expect(element(by.id("pageNumber0"))).toBeVisible();
-    await element(by.id('pager-view')).swipe("left","fast");
-    await expect(element(by.id("pageNumber1"))).toBeVisible();
+    await expect(element(by.id('pageNumber0'))).toBeVisible();
+    await element(by.id('pager-view')).swipe('left', 'fast');
+    await expect(element(by.id('pageNumber1'))).toBeVisible();
   });
-
 });
-
