55e98f063243614b1b14340a3a88a1a6ec0de7bc
Danilo Bürger <danilo@feastr.de>
2021-12-22 14:54:16 +0100

    Prevent 1 frame flicker on android if initialPage != 0

--- android/src/main/java/com/reactnativepagerview/PagerViewViewManager.kt
+++ android/src/main/java/com/reactnativepagerview/PagerViewViewManager.kt
@@ -96,6 +96,11 @@ class PagerViewViewManager : ViewGroupManager<NestedScrollableHost>() {
       // https://github.com/facebook/react-native/issues/17968#issuecomment-697136929
       refreshViewChildrenLayout(parent)
     }
+
+    if (!host.didSetInitialIndex && host.initialIndex == index) {
+      host.didSetInitialIndex = true
+      setCurrentItem(parent, index, false)
+    }
   }
 
   override fun getChildCount(parent: NestedScrollableHost) = getViewPager(parent).adapter?.itemCount ?: 0
@@ -148,9 +153,9 @@ class PagerViewViewManager : ViewGroupManager<NestedScrollableHost>() {
     //https://github.com/callstack/react-native-pager-view/issues/456
     //Initial index should be set only once. 
     if (host.initialIndex === null) {
+      host.initialIndex = value
       view.post {
-        setCurrentItem(view, value, false)
-        host.initialIndex = value
+        host.didSetInitialIndex = true
       }
     }
   }
