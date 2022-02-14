callstack/react-native-pager-view

commit d44b58a95e783fc990a3eaae1917d6cb801fff5f

fix(ios): multiple onPageSelected calling (#455)

ios/ReactNativePageView.m
@@ -159,14 +159,16 @@
         [self setReactViewControllers:self.initialPage
                                  with:initialController
                             direction:UIPageViewControllerNavigationDirectionForward
-                             animated:YES];
+                             animated:YES
+             shouldCallOnPageSelected:YES];
     }
 }
 
 - (void)setReactViewControllers:(NSInteger)index
                            with:(UIViewController *)controller
                       direction:(UIPageViewControllerNavigationDirection)direction
-                       animated:(BOOL)animated {
+                       animated:(BOOL)animated
+                       shouldCallOnPageSelected:(BOOL)shouldCallOnPageSelected {
     if (self.reactPageViewController == nil) {
         return;
     }
@@ -183,7 +185,9 @@
         
         if (strongSelf.eventDispatcher) {
             if (strongSelf.lastReportedIndex != strongSelf.currentIndex) {
-                [strongSelf.eventDispatcher sendEvent:[[RCTOnPageSelected alloc] initWithReactTag:strongSelf.reactTag position:@(index) coalescingKey:coalescingKey]];
+                if (shouldCallOnPageSelected) {
+                    [strongSelf.eventDispatcher sendEvent:[[RCTOnPageSelected alloc] initWithReactTag:strongSelf.reactTag position:@(index) coalescingKey:coalescingKey]];
+                }
                 strongSelf.lastReportedIndex = strongSelf.currentIndex;
             }
         }
@@ -236,33 +240,39 @@
     long diff = labs(index - _currentIndex);
     
     if (isForward && diff > 0) {
-        for (NSInteger i=_currentIndex+1; i<=index; i++) {
-            [self goToViewController:i direction:direction animated:animated];
+        for (NSInteger i=_currentIndex; i<=index; i++) {
+            if (i == _currentIndex) {
+                continue;
+            }
+            [self goToViewController:i direction:direction animated:animated shouldCallOnPageSelected: i == index];
         }
     }
     
     if (!isForward && diff > 0) {
-        for (NSInteger i=_currentIndex-1; i>=index; i--) {
-            if (i >=0) {
-                [self goToViewController:i direction:direction animated:animated];
+        for (NSInteger i=_currentIndex; i>=index; i--) {
+            if (index == _currentIndex) {
+                continue;
             }
+            [self goToViewController:i direction:direction animated:animated shouldCallOnPageSelected: i == index];
         }
     }
     
     if (diff == 0) {
-        [self goToViewController:index direction:direction animated:animated];
+        [self goToViewController:index direction:direction animated:animated shouldCallOnPageSelected:YES];
     }
 }
 
 - (void)goToViewController:(NSInteger)index
                             direction:(UIPageViewControllerNavigationDirection)direction
-                            animated:(BOOL)animated {
+                            animated:(BOOL)animated
+                            shouldCallOnPageSelected:(BOOL)shouldCallOnPageSelected {
     UIView *viewToDisplay = self.reactSubviews[index];
     UIViewController *controllerToDisplay = [self findAndCacheControllerForView:viewToDisplay];
     [self setReactViewControllers:index
                              with:controllerToDisplay
                         direction:direction
-                         animated:animated];
+                         animated:animated
+                        shouldCallOnPageSelected:shouldCallOnPageSelected];
 }
     
 - (UIViewController *)findAndCacheControllerForView:(UIView *)viewToDisplay {
@@ -320,7 +330,6 @@
         self.currentIndex = currentIndex;
         self.currentView = currentVC.view;
         self.reactPageIndicatorView.currentPage = currentIndex;
-        
         [self.eventDispatcher sendEvent:[[RCTOnPageSelected alloc] initWithReactTag:self.reactTag position:@(currentIndex) coalescingKey:_coalescingKey++]];
         [self.eventDispatcher sendEvent:[[RCTOnPageScrollEvent alloc] initWithReactTag:self.reactTag position:@(currentIndex) offset:@(0.0)]];
         self.lastReportedIndex = currentIndex;

example/src/App.tsx
@@ -54,6 +54,7 @@ function App() {
           key={example.name}
           style={styles.exampleTouchable}
           onPress={() => {
+            //@ts-ignore
             navigation.navigate(example.name);
           }}
         >

commit 715fe70349224dcecc1b612e2e272bc336a6e244

fix(ios): wrong variable usage that causes crash related to #458 (#502)
Introduced in #455 this should have been `i` instead of `index` all along.

ios/ReactNativePageView.m
@@ -265,7 +265,7 @@
     if (!isForward && diff > 0) {
         for (NSInteger i=_currentIndex; i>=index; i--) {
             // Prevent removal of one or many pages at a time
-            if (index == _currentIndex || i >= numberOfPages) {
+            if (i == _currentIndex || i >= numberOfPages) {
                 continue;
             }
             [self goToViewController:i direction:direction animated:animated shouldCallOnPageSelected: i == index];