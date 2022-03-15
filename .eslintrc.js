36d355b69be18a17a5b3b3d30f05e180c6ebd309
Piotr Trocki <piotr@trocki.com>
2022-03-15 09:41:06 +0100

    update package.json eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,18 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community'],
+  plugins: ['@react-native/eslint-plugin-specs'],
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    '@react-native/specs/react-native-modules': 'error',
+  },
+  overrides: [
+    {
+      files: ['src/specs/**/*.js'],
+      rules: {
+        '@react-native/specs/react-native-modules': 'error',
+      },
+    },
+  ],
   globals: {
     expect: true,
     element: true,
