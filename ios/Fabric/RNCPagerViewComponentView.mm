11a52e2e7fe29fadfb805bd02131b31a3aae0e91
Piotr Trocki <piotr@trocki.com>
2022-03-18 15:54:18 +0100

    implement events

--- ios/Fabric/RNCPagerViewComponentView.mm
+++ ios/Fabric/RNCPagerViewComponentView.mm
@@ -180,26 +180,18 @@ using namespace facebook::react;
     if (_nativePageViewController == nil) {
         return;
     }
+
     __weak RNCPagerViewComponentView *weakSelf = self;
-//    uint16_t coalescingKey = _coalescingKey++;
-    
     [_nativePageViewController setViewControllers:@[[_nativeChildrenViewControllers objectAtIndex:index]]
                                            direction:direction
                                             animated:animated
                                           completion:^(BOOL finished) {
-        //TODO
-//        __strong typeof(self) strongSelf = weakSelf;
-//        strongSelf.currentIndex = index;
-//        strongSelf.currentView = controller.view;
-//
-//        if (strongSelf.eventDispatcher) {
-//            if (strongSelf.lastReportedIndex != strongSelf.currentIndex) {
-//                if (shouldCallOnPageSelected) {
-//                    [strongSelf.eventDispatcher sendEvent:[[RCTOnPageSelected alloc] initWithReactTag:strongSelf.reactTag position:@(index) coalescingKey:coalescingKey]];
-//                }
-//                strongSelf.lastReportedIndex = strongSelf.currentIndex;
-//            }
-//        }
+        __strong typeof(self) strongSelf = weakSelf;
+        if (strongSelf->_eventEmitter != nullptr && shouldCallOnPageSelected) {
+            const auto strongEventEmitter = *std::dynamic_pointer_cast<const RNCViewPagerEventEmitter>(strongSelf->_eventEmitter);
+            int position = (int) index;
+            strongEventEmitter.onPageSelected(RNCViewPagerEventEmitter::OnPageSelected{position : position});
+        }
     }];
 }
 
@@ -226,20 +218,74 @@ using namespace facebook::react;
     return _nativePageViewController.viewControllers.firstObject;
 }
 
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    const auto strongEventEmitter = *std::dynamic_pointer_cast<const RNCViewPagerEventEmitter>(_eventEmitter);
+    strongEventEmitter.onPageScrollStateChanged(RNCViewPagerEventEmitter::OnPageScrollStateChanged{pageScrollState: RNCViewPagerEventEmitter::OnPageScrollStateChangedPageScrollState::Dragging });
+}
+
+- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset {
+    const auto strongEventEmitter = *std::dynamic_pointer_cast<const RNCViewPagerEventEmitter>(_eventEmitter);
+    strongEventEmitter.onPageScrollStateChanged(RNCViewPagerEventEmitter::OnPageScrollStateChanged{pageScrollState: RNCViewPagerEventEmitter::OnPageScrollStateChangedPageScrollState::Settling });
+// TODO
+//    if (!_overdrag) {
+//        NSInteger maxIndex = _reactPageIndicatorView.numberOfPages - 1;
+//        BOOL isFirstPage = [self isLtrLayout] ? _currentIndex == 0 : _currentIndex == maxIndex;
+//        BOOL isLastPage = [self isLtrLayout] ? _currentIndex == maxIndex : _currentIndex == 0;
+//        CGFloat contentOffset =[self isHorizontal] ? scrollView.contentOffset.x : scrollView.contentOffset.y;
+//        CGFloat topBound = [self isHorizontal] ? scrollView.bounds.size.width : scrollView.bounds.size.height;
+//
+//        if ((isFirstPage && contentOffset <= topBound) || (isLastPage && contentOffset >= topBound)) {
+//            CGPoint croppedOffset = [self isHorizontal] ? CGPointMake(topBound, 0) : CGPointMake(0, topBound);
+//            *targetContentOffset = croppedOffset;
+//        }
+//    }
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    const auto strongEventEmitter = *std::dynamic_pointer_cast<const RNCViewPagerEventEmitter>(_eventEmitter);
+    strongEventEmitter.onPageScrollStateChanged(RNCViewPagerEventEmitter::OnPageScrollStateChanged{pageScrollState: RNCViewPagerEventEmitter::OnPageScrollStateChangedPageScrollState::Idle });
+}
+
+- (BOOL)isHorizontal {
+    return _nativePageViewController.navigationOrientation == UIPageViewControllerNavigationOrientationHorizontal;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGPoint point = scrollView.contentOffset;
+    float offset = 0;
+    if (self.isHorizontal) {
+        if (self.frame.size.width != 0) {
+            offset = (point.x - self.frame.size.width)/self.frame.size.width;
+        }
+    } else {
+        if (self.frame.size.height != 0) {
+            offset = (point.y - self.frame.size.height)/self.frame.size.height;
+        }
+    }
+    
+    float absoluteOffset = fabs(offset);
+    NSInteger position = self.currentIndex;
+    const auto strongEventEmitter = *std::dynamic_pointer_cast<const RNCViewPagerEventEmitter>(_eventEmitter);
+    int eventPosition = (int) position;
+    strongEventEmitter.onPageScroll(RNCViewPagerEventEmitter::OnPageScroll{position: eventPosition, offset: absoluteOffset});
+}
+
 #pragma mark - UIPageViewControllerDelegate
 
 - (void)pageViewController:(UIPageViewController *)pageViewController
         didFinishAnimating:(BOOL)finished
    previousViewControllers:(nonnull NSArray<UIViewController *> *)previousViewControllers
        transitionCompleted:(BOOL)completed {
-    if(finished){
-        NSLog(@"_currentIndex %ld",(long)_currentIndex);
-    }
     if (completed) {
         UIViewController* currentVC = [self currentlyDisplayed];
         NSUInteger currentIndex = [_nativeChildrenViewControllers indexOfObject:currentVC];
         _currentIndex = currentIndex;
-        NSLog(@"_currentIndex %ld",(long)_currentIndex);
+        int position = (int) currentIndex;
+        const auto strongEventEmitter = *std::dynamic_pointer_cast<const RNCViewPagerEventEmitter>(_eventEmitter);
+        strongEventEmitter.onPageSelected(RNCViewPagerEventEmitter::OnPageSelected{position : position});
+        strongEventEmitter.onPageScroll(RNCViewPagerEventEmitter::OnPageScroll{position: position, offset: 0.0});
     }
 }
 
--- src/spec/NativePagerView.js
+++ src/spec/NativePagerView.js
@@ -2,24 +2,43 @@
  * @flow strict-local
  */
 
-import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-import type {
-  WithDefault,
-  Int32,
-} from 'react-native/Libraries/Types/CodegenTypes';
-import type { ViewProps } from 'react-native/Libraries/Components/View/ViewPropTypes';
-
-//TODO implement all types here
-type NativeProps = $ReadOnly<{|
-  ...ViewProps,
-  initialPage?: WithDefault<Int32, 0>,
-  pageMargin?: WithDefault<Int32, 0>,
-  keyboardDismissMode?: WithDefault<'none' | 'on-drag', 'none'>,
-  orientation?: WithDefault<'horizontal' | 'vertical', 'horizontal'>,
-  overdrag?: boolean,
-  scrollEnabled?: boolean,
+ import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
+ import type {
+   WithDefault,
+   Int32,
+   Float,
+   BubblingEventHandler,
+ } from 'react-native/Libraries/Types/CodegenTypes';
+ import type { ViewProps } from 'react-native/Libraries/Components/View/ViewPropTypes';
+ import type { HostComponent } from 'react-native/Libraries/Renderer/shims/ReactNativeTypes';
+ 
+ type PageScrollEvent = $ReadOnly<{|
+    position: Int32,
+    offset: Float,
 |}>;
 
-export default (codegenNativeComponent<NativeProps>(
-  'RNCViewPager'
-): HostComponent<NativeProps>);
+type PageSelectedEvent = $ReadOnly<{|
+  position: Int32,
+|}>;
+
+type PageScrollStateChangedEvent = $ReadOnly<{|
+  pageScrollState: 'idle' | 'dragging' | 'settling',
+|}>;
+
+ type NativeProps = $ReadOnly<{|
+   ...ViewProps,
+   onPageScroll?: ?BubblingEventHandler<PageScrollEvent>,
+  onPageSelected?: ?BubblingEventHandler<PageSelectedEvent>,
+  onPageScrollStateChanged?: ?BubblingEventHandler<PageScrollStateChangedEvent>,
+   initialPage?: WithDefault<Int32, 0>,
+   pageMargin?: WithDefault<Int32, 0>,
+   keyboardDismissMode?: WithDefault<'none' | 'on-drag', 'none'>,
+   orientation?: WithDefault<'horizontal' | 'vertical', 'horizontal'>,
+   overdrag?: boolean,
+  scrollEnabled?: boolean,
+ |}>;
+ 
+ export default (codegenNativeComponent<NativeProps>(
+   'RNCViewPager'
+ ): HostComponent<NativeProps>);
+ 
