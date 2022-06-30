d35cf6e4cbab0906c7587246972719f8dd27cbf5
Andrei Alecu <andreialecu@users.noreply.github.com>
2022-06-30 19:17:22 +0300

    fix(ios): incorrect pager height

--- ios/ReactNativePageView.h
+++ ios/ReactNativePageView.h
@@ -23,7 +23,6 @@ NS_ASSUME_NONNULL_BEGIN
 @property(nonatomic, copy) RCTDirectEventBlock onPageScrollStateChanged;
 @property(nonatomic) BOOL overdrag;
 @property(nonatomic) NSString* layoutDirection;
-@property(nonatomic) CGRect previousBounds;
 @property(nonatomic, assign) BOOL animating;
 
 - (void)goTo:(NSInteger)index animated:(BOOL)animated;
