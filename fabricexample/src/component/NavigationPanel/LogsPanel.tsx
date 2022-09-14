309b7716e82a923e6a8b2c6fa4a544b26bc52bb4
Piotr Trocki <piotr.trocki@callstack.com>
2022-09-14 16:13:44 +0200

    fix: typescript

--- fabricexample/src/component/NavigationPanel/LogsPanel.tsx
+++ fabricexample/src/component/NavigationPanel/LogsPanel.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import type { LogsPanelProps } from './types';
 
 export function LogsPanel({ logs }: LogsPanelProps) {
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,13 @@
+export * from './types';
 import { PagerView } from './PagerView';
 export default PagerView;
+
+import type { PagerViewOnPageScrollEventData } from './PagerViewViewNativeComponent';
+import type { PagerViewOnPageSelectedEventData } from './PagerViewViewNativeComponent';
+import type { PageScrollStateChangedEvent } from './PagerViewViewNativeComponent';
+
+export {
+  PagerViewOnPageScrollEventData,
+  PagerViewOnPageSelectedEventData,
+  PageScrollStateChangedEvent,
+};
