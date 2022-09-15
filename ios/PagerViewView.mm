a89622af664cf4758d5322294b8a722a28ef12f0
krozniata <56474758+krozniata@users.noreply.github.com>
2022-09-15 13:25:09 +0200

    feat(ios): add legacy code & change codegen name

--- ios/PagerViewView.mm
+++ ios/PagerViewView.mm
@@ -1,16 +1,16 @@
 #ifdef RCT_NEW_ARCH_ENABLED
 #import "PagerViewView.h"
 
-#import <react/renderer/components/PagerViewView/ComponentDescriptors.h>
-#import <react/renderer/components/PagerViewView/EventEmitters.h>
-#import <react/renderer/components/PagerViewView/Props.h>
-#import <react/renderer/components/PagerViewView/RCTComponentViewHelpers.h>
+#import <react/renderer/components/RNCViewPager/ComponentDescriptors.h>
+#import <react/renderer/components/RNCViewPager/EventEmitters.h>
+#import <react/renderer/components/RNCViewPager/Props.h>
+#import <react/renderer/components/RNCViewPager/RCTComponentViewHelpers.h>
 
 #import "RCTFabricComponentsPlugins.h"
 
 using namespace facebook::react;
 
-@interface PagerViewView () <RCTPagerViewViewViewProtocol>
+@interface PagerViewView () <RCTRNCViewPagerViewProtocol>
 
 @end
 
@@ -20,13 +20,13 @@ using namespace facebook::react;
 
 + (ComponentDescriptorProvider)componentDescriptorProvider
 {
-    return concreteComponentDescriptorProvider<PagerViewViewComponentDescriptor>();
+    return concreteComponentDescriptorProvider<RNCViewPagerComponentDescriptor>();
 }
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
     if (self = [super initWithFrame:frame]) {
-        static const auto defaultProps = std::make_shared<const PagerViewViewProps>();
+        static const auto defaultProps = std::make_shared<const RNCViewPagerProps>();
         _props = defaultProps;
         
         _view = [[UIView alloc] init];
@@ -39,20 +39,20 @@ using namespace facebook::react;
 
 - (void)updateProps:(Props::Shared const &)props oldProps:(Props::Shared const &)oldProps
 {
-    const auto &oldViewProps = *std::static_pointer_cast<PagerViewViewProps const>(_props);
-    const auto &newViewProps = *std::static_pointer_cast<PagerViewViewProps const>(props);
+    const auto &oldViewProps = *std::static_pointer_cast<RNCViewPagerProps const>(_props);
+    const auto &newViewProps = *std::static_pointer_cast<RNCViewPagerProps const>(props);
     
     [super updateProps:props oldProps:oldProps];
 }
 
-Class<RCTComponentViewProtocol> PagerViewViewCls(void)
+Class<RCTComponentViewProtocol> RNCViewPagerCls(void)
 {
     return PagerViewView.class;
 }
 
 - (void)handleCommand:(const NSString *)commandName args:(const NSArray *)args
 {
-    RCTPagerViewViewHandleCommand(self, commandName, args);
+    RCTRNCViewPagerHandleCommand(self, commandName, args);
 }
 
 - (void)setPage:(NSInteger)selectedPage {
