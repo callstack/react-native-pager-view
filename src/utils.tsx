15ce384f976b0764ccf4349a943902e712a94c59
Sanjay <sanjay.patidar@bswhealth.org>
2022-10-11 11:28:54 -0500

    fixed issue when pagerView is inside the scrollView and views does not get displayed/scrolled due to position 'absolute'

--- src/utils.tsx
+++ src/utils.tsx
@@ -16,11 +16,11 @@ export const childrenWithOverriddenStyle = (children?: ReactNode) => {
     const element = child as React.ReactElement;
     return (
       // Add a wrapper to ensure layout is calculated correctly
-      <View style={StyleSheet.absoluteFill} collapsable={false}>
+      <View style={[StyleSheet.absoluteFill, { position: element.props.style?.position ?? 'absolute' }]} collapsable={false}>
         {React.cloneElement(element, {
           ...element.props,
           // Override styles so that each page will fill the parent.
-          style: [element.props.style, StyleSheet.absoluteFill],
+          style: [StyleSheet.absoluteFill, element.props.style],
         })}
       </View>
     );
