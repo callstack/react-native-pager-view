31d5b31ab9f8ee8b75d11dfa34046b506df22a34
Jan Jaworski <jaworek3211@gmail.com>
2023-03-27 11:53:42 +0200

    ignore error

--- src/PagerView.tsx
+++ src/PagerView.tsx
@@ -147,6 +147,7 @@ export class PagerView extends React.Component<PagerViewProps> {
       <PagerViewView
         {...this.props}
         ref={(ref) => {
+          //@ts-ignore fix it
           this.pagerView = ref;
         }}
         style={this.props.style}
