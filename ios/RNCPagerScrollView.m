aee272ce5e0ae792da0ceef6a67eb6e3fbd2991f
Nishan <nishanbende@gmail.com>
2023-03-28 08:27:00 +0530

    nit: comment

--- ios/RNCPagerScrollView.m
+++ ios/RNCPagerScrollView.m
@@ -16,6 +16,7 @@
         UIUserInterfaceLayoutDirection layoutDirection = [UIView userInterfaceLayoutDirectionForSemanticContentAttribute:self.semanticContentAttribute];
         BOOL isLTR = UIUserInterfaceLayoutDirectionLeftToRight == layoutDirection;
         BOOL isBackGesture = (isLTR && velocity.x > 0) || (!isLTR && velocity.x < 0);
+        // if it's back gesture and scroll view is at the beginning of scroll we allow simultaneous gesture
         if (isBackGesture && self.contentOffset.x <= 0) {
             return YES;
         }
