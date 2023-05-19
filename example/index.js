ef5793b902334697b756f477f92d5bc92c5d42a3
Piotr Trocki <piotr.trocki@callstack.com>
2023-05-19 12:51:07 +0200

    fix android

--- example/index.js
+++ example/index.js
@@ -1,5 +1,12 @@
 import React, { useMemo } from 'react';
-import { AppRegistry, View, SafeAreaView, Animated, Text } from 'react-native';
+import {
+  AppRegistry,
+  View,
+  SafeAreaView,
+  Animated,
+  Text,
+  Platform,
+} from 'react-native';
 import { Navigation } from './src/App';
 import { name as appName } from './app.json';
 import { BasicPagerViewExample } from './src/BasicPagerViewExample';
@@ -9,42 +16,12 @@ import { LikeCount } from './src/component/LikeCount';
 import { NavigationPanel } from './src/component/NavigationPanel';
 import { PaperExample } from './src/PaperExample';
 
-// AppRegistry.registerComponent('Paper', () => (props) => {
-//   const { ref, ...navigationPanel } = useNavigationPanel();
-//   return (
-//     <PagerView
-//       //@ts-ignore
-//       testID="pager-view"
-//       ref={ref}
-//       style={{ flex: 1 }}
-//       initialPage={0}
-//       layoutDirection="ltr"
-//       pageMargin={10}
-//       // Lib does not support dynamically orientation change
-//       orientation="horizontal"
-//     >
-//       {useMemo(
-//         () =>
-//           navigationPanel.pages.map((page, index) => (
-//             <View key={page.key} style={page.style} collapsable={false}>
-//               <LikeCount />
-//               <Text
-//                 testID={`pageNumber${index}`}
-//               >{`page number ${index}`}</Text>
-//             </View>
-//           )),
-//         [navigationPanel.pages]
-//       )}
-//     </PagerView>
-//   );
-// });
-
-// AppRegistry.registerComponent('Paper', () => (props) => {
-//   const { ref, ...navigationPanel } = useNavigationPanel();
-//   return <LikeCount />;
-// });
-
 AppRegistry.registerComponent(appName, () => Navigation);
 AppRegistry.registerComponent('Paper', () => (props) => {
-  return <PaperExample />;
+  // https://github.com/software-mansion/react-native-reanimated/issues/3820 - Android workaround
+  return Platform.OS === 'android' ? (
+    <PaperExample />
+  ) : (
+    <BasicPagerViewExample />
+  );
 });
