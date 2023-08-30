f23a5da98fa6f8cd00af15b9bfc05e5c1d024a4a
Piotr Trocki <piotr.trocki@callstack.com>
2023-08-30 19:50:43 +0200

    turn off new architecture

--- example/index.js
+++ example/index.js
@@ -1,10 +1,5 @@
 import { AppRegistry } from 'react-native';
 import { Navigation } from './src/App';
-import { BasicPagerViewExample } from './src/BasicPagerViewExample';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => BasicPagerViewExample);
-
-// FOR SOME REASON REANIMATED OR GH IS BROKEN FOR 72
-// Uncomment it if more examples are needed
-// AppRegistry.registerComponent(appName, () => Navigation);
+AppRegistry.registerComponent(appName, () => Navigation);
