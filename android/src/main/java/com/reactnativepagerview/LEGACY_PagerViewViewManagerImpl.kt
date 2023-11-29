6a54d0a89dfdd96878e86f55c284abb81573f190
Igor Bejnarowicz <igor.bejnarowicz@callstack.com>
2023-11-29 07:47:22 +0100

    refactor(android): extract module name to shared variable, add comment for context

---
+++ android/src/main/java/com/reactnativepagerview/LEGACY_PagerViewViewManagerImpl.kt
@@ -0,0 +1,7 @@
+package com.reactnativepagerview
+
+// Note: The LEGACY_ variant is an iOS-only feature and the related Android files
+// are only included because of and relevant to auxiliary processes, like Codegen.
+object LEGACY_PagerViewViewManagerImpl {
+    const val NAME = "LEGACY_RNCViewPager"
+}
--- android/src/paper/java/com/reactnativepagerview/LEGACY_PagerViewViewManager.kt
+++ android/src/paper/java/com/reactnativepagerview/LEGACY_PagerViewViewManager.kt
@@ -5,10 +5,11 @@ import android.widget.FrameLayout
 import com.facebook.react.module.annotations.ReactModule
 import com.facebook.react.uimanager.ThemedReactContext
 
-
-@ReactModule(name = "LEGACY_RNCViewPager")
+// Note: The LEGACY_ variant is an iOS-only feature and the related Android files
+// are only included because of and relevant to auxiliary processes, like Codegen.
+@ReactModule(name = LEGACY_PagerViewViewManagerImpl.NAME)
 class LEGACY_PagerViewViewManager : ViewGroupManager<FrameLayout>() {
-    override fun getName() = "LEGACY_RNCViewPager"
+    override fun getName() = LEGACY_PagerViewViewManagerImpl.NAME
 
     override fun createViewInstance(context: ThemedReactContext): FrameLayout {
         throw Error("LEGACY_RNCViewPager is an iOS-only feature")
